cc3e2ba7193bce6a77f8c910482eeb1c524be0d2
Sebastian Cruz Go <sebastia.cruzgo@hes-so.ch>
2024-01-16 12:07:55 +0100

    Token of player 1 trimmed

---
+++ src/main/scala/Audio.scala
@@ -0,0 +1,28 @@
+import javax.sound.sampled.{AudioSystem, Clip}
+
+class Audio (path: String) {
+  var audioClip: Clip = _
+
+  try {
+    val url = classOf[Audio].getResource(path)
+    val audioStream = AudioSystem.getAudioInputStream(url)
+
+    audioClip = AudioSystem.getClip.asInstanceOf[Clip]
+    audioClip.open(audioStream)
+  } catch {
+    case e: Exception =>
+      e.printStackTrace()
+  }
+
+  def play(): Unit = {
+    try {
+      if (!audioClip.isOpen) audioClip.open()
+      audioClip.stop()
+      audioClip.setMicrosecondPosition(0)
+      audioClip.start()
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+  }
+}
